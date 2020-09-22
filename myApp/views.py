821438578fed6b562cc79fd55b0c36c3c5b39324
= <=>
2020-09-22 09:54:28 +0700

    22092020

--- myApp/views.py
+++ myApp/views.py
@@ -13,9 +13,9 @@ class HomeView(ListView):
     ordering = ['-created_at']
 
     def get_context_data(self, *args, **kwargs):
-        cat_menu = Catagory.objects.all()
-        context = super(HomeView, self).get_context_data(*args, **kwargs)
-        context["cat_menu"] = cat_menu
+        # cat_menu = Catagory.objects.all()
+        context = super().get_context_data(*args, **kwargs)
+        context["cat_menu"] = Catagory.objects.all()
         return context
 
 def CatagoryListView(request):
@@ -32,7 +32,7 @@ class ArticleDetailView(DetailView):
 
     def get_context_data(self, *args, **kwargs):
         cat_menu = Catagory.objects.all()
-        context = super(ArticleDetailView, self).get_context_data(*args, **kwargs)
+        context = super().get_context_data(*args, **kwargs)
         context["cat_menu"] = cat_menu
         return context
 
